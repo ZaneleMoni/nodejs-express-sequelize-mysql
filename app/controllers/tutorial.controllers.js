fbfce6cef07a893c8b9f3bfcd0c827d4b40070d2
Zanele Moni <zanelemoni4@gmail.com>
2022-02-15 08:59:16 +0200

    my commits

---
+++ app/controllers/tutorial.controllers.js
@@ -0,0 +1,146 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+//CREATE AND SAVE A NEW OBJECT
+exports.createTutorial = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+  // Create a Tutorial
+  const tutorial = {
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false,
+  };
+  // Save Tutorial in the database
+  Tutorial.create(tutorial)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial.",
+      });
+    });
+};
+
+//RETRIEVE OBJECTS WITH CONDITIONS
+exports.findAllTutorials = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  Tutorial.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+//RETRIEVE A SINGLE OBJECT
+exports.findOneTutorial = (req, res) => {
+  const id = req.params.id;
+  Tutorial.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Tutorial with id=${id}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id,
+      });
+    });
+};
+
+//UPDATE AN OBJECT
+exports.updateTutorial = (req, res) => {
+  const id = req.params.id;
+  Tutorial.update(req.body, {
+    where: { id: id },
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id,
+      });
+    });
+};
+
+//DELETE AN OBJECT
+exports.deleteTutorial = (req, res) => {
+  const id = req.params.id;
+  Tutorial.destroy({
+    where: { id: id },
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Tutorial was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id=" + id,
+      });
+    });
+};
+
+//DELETE ALL OBJECT
+exports.deleteAllTutorials = (req, res) => {
+  Tutorial.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} Tutorials were deleted successfully!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials.",
+      });
+    });
+};
+
+//FIND ALL OBJECTS BY CONDITION
+exports.findAllPublishedTutorials = (req, res) => {
+  Tutorial.findAll({ where: { published: true } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
